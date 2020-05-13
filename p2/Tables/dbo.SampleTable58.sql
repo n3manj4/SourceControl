b66fc1e9e9cae5e52e61fb9f487aaf48acb2f233
n3manj4 <>
2020-05-13 20:15:04 +0200

    add

--- p2/Tables/dbo.SampleTable58.sql
+++ p2/Tables/dbo.SampleTable58.sql
@@ -3,12 +3,13 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[SampleTable58] (
-		[PersonID]       [int] NULL,
-		[LastName]       [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[FirstName]      [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[Address]        [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[City]           [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new]     [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[PersonID]         [int] NULL,
+		[LastName]         [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[FirstName]        [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Address]          [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[City]             [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[column_new]       [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[columrrn_new]     [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
 ALTER TABLE [dbo].[SampleTable58] SET (LOCK_ESCALATION = TABLE)
