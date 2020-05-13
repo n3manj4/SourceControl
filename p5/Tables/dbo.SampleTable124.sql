5f2f3f44f360db7e5c77acb311b8ab1405180dab
n3manj4 <>
2020-05-13 17:33:54 +0200

    init

---
+++ p5/Tables/dbo.SampleTable124.sql
@@ -0,0 +1,14 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[SampleTable124] (
+		[PersonID]      [int] NULL,
+		[LastName]      [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[FirstName]     [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Address]       [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[City]          [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+GO
+ALTER TABLE [dbo].[SampleTable124] SET (LOCK_ESCALATION = TABLE)
+GO
