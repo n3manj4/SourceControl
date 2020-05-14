3b347e4a66ce4b868fc14151cd22fe5411f96c94
n3manj4 <>
2020-05-14 13:04:43 +0200

    zxcvb

--- p6/Tables/dbo.SampleTable219.sql
+++ p6/Tables/dbo.SampleTable219.sql
@@ -4,16 +4,15 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[SampleTable219] (
 		[PersonID]          [int] NULL,
-		[LastName]          [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[FirstName]         [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[Address]           [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[City]              [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new]        [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[columnnn_new]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[columnnnn_new]     [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[colasdf_new]       [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[coqwer_new]        [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[LastName]          [varchar](255) NULL,
+		[FirstName]         [varchar](255) NULL,
+		[Address]           [varchar](255) NULL,
+		[City]              [varchar](255) NULL,
+		[column_new]        [varchar](1) NULL,
+		[columnnn_new]      [varchar](1) NULL,
+		[columnnnn_new]     [varchar](1) NULL,
+		[colasdf_new]       [varchar](1) NULL,
+		[coqwer_new]        [varchar](1) NULL,
+		[coaaa_new]         [varchar](1) NULL
 )
-GO
-ALTER TABLE [dbo].[SampleTable219] SET (LOCK_ESCALATION = TABLE)
 GO
