969f8398ebc6704ace97c8b5e65b124402a613ca
n3manj4 <>
2020-05-15 19:39:03 +0200

    gdfsgsdf

--- p7/Tables/dbo.SampleTable158.sql
+++ p7/Tables/dbo.SampleTable158.sql
@@ -1,19 +1,8 @@
 SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
-SET ANSI_PADDING ON
+SET ANSI_PADDING OFF
 GO
-CREATE TABLE [dbo].[SampleTable158] (
-		[PersonID]         [int] NULL,
-		[LastName]         [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[FirstName]        [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[Address]          [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[City]             [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new1]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new2]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new3]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[wwcolumn_new]     [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new4]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-)
+CREATE TABLE [dbo].[SampleTable158]
 GO
 ALTER TABLE [dbo].[SampleTable158] SET (LOCK_ESCALATION = TABLE)
 GO
