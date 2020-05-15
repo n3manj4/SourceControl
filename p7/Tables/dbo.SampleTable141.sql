b5529d0809c0cc15f6def97e3df66c42ff453ec6
n3manj4 <>
2020-05-15 18:11:42 +0200

    xgvdsg

--- p7/Tables/dbo.SampleTable141.sql
+++ p7/Tables/dbo.SampleTable141.sql
@@ -8,7 +8,8 @@ CREATE TABLE [dbo].[SampleTable141] (
 		[FirstName]        [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Address]          [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[City]             [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[wwcolumn_new]     [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[wwcolumn_new]     [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[column_new1]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
 ALTER TABLE [dbo].[SampleTable141] SET (LOCK_ESCALATION = TABLE)
