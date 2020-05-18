d55f84c71109ecf5bff5b6b0f20326788990d9a4
n3manj4 <>
2020-05-18 12:43:17 +0200

    zxvvzx

--- p7/Tables/dbo.SampleTable105.sql
+++ p7/Tables/dbo.SampleTable105.sql
@@ -9,15 +9,7 @@ CREATE TABLE [dbo].[SampleTable105] (
 		[Address]          [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[City]             [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[wwcolumn_new]     [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new1]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new5]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new6]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new2]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new3]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new4]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new8]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new9]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new10]     [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[column_new1]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
 ALTER TABLE [dbo].[SampleTable105] SET (LOCK_ESCALATION = TABLE)
