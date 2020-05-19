e1b02213b6e8c8171aa1af35afa1f592dde0023f
n3manj4 <>
2020-05-19 16:29:58 +0200

    sfgfdg

--- p8/Tables/dbo.SampleTable100.sql
+++ p8/Tables/dbo.SampleTable100.sql
@@ -8,6 +8,7 @@ CREATE TABLE [dbo].[SampleTable100] (
 		[FirstName]       [varchar](255) NULL,
 		[Address]         [varchar](255) NULL,
 		[City]            [varchar](255) NULL,
-		[column_new1]     [varchar](1) NULL
+		[column_new1]     [varchar](1) NULL,
+		[column_new2]     [varchar](1) NULL
 )
 GO
