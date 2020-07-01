003f7b05c34e7dc814d58c3fc136a97d16d5bbbc
n3manj4 <nemanjaks4@gmail.com>
2020-07-01 13:28:19 +0200

    Update dbo.SampleTable100.sql

--- p9/Tables/dbo.SampleTable100.sql
+++ p9/Tables/dbo.SampleTable100.sql
@@ -4,7 +4,7 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[SampleTable100] (
 		[PersonID]         [int] NULL,
-		[LastName]         [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[LastName]         [varchar](200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[FirstName]        [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Address]          [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[City]             [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
