83bd04b7ea5e9deb59635601df63b0853b4c36dd
n3manj4 <nemanjaks4@gmail.com>
2020-07-01 13:02:27 +0200

    Update dbo.SampleTable100.sql

--- p9/Tables/dbo.SampleTable100.sql
+++ p9/Tables/dbo.SampleTable100.sql
@@ -8,7 +8,7 @@ CREATE TABLE [dbo].[SampleTable100] (
 		[FirstName]        [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Address]          [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[City]             [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[column_new1]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[column_new1new]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[column_new2]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[column_new3]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[column_new5]      [varchar](1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
