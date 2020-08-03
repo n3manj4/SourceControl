c50560c5df001a9dbc357ceaebff15c7ec8f8d4c
n3manj4 <nemanjaks4@gmail.com>
2020-08-03 15:43:59 +0200

    Create Production.ScrapReason.sql

---
+++ p1/Tables/Production.ScrapReason.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA [Production]
+	AUTHORIZATION [dbo]asdfdsafsadf
+GO
+EXEC sp_addextendedprasdfsadfsdafsdafasfoperty N'MS_Description', N'Contains objects related to products, inventory, and manufacturing.', 'SCHEMA', N'Production', NULL, NULL, NULL, NULL
+GO
