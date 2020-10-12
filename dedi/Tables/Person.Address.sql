747bc5192474b1a1246d404fc91f1ec2b639ff8b
n3manj4 <nemanjaks4@gmail.com>
2020-10-12 11:04:00 +0200

    Update Person.Address.sql

--- dedi/Tables/Person.Address.sql
+++ dedi/Tables/Person.Address.sql
@@ -20,7 +20,7 @@ CREATE TABLE [Person].[Address] (
 	ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-EXEC sp_addextendedproperty N'MS_Description', N'Clustered index created by a primary key constraint.', 'SCHEMA', N'Person', 'TABLE', N'Address', 'INDEX', N'PK_Address_AddressID'
+EXEC sp_addextendedproperty N'MS_Descriptionnnnn', N'Clustered index created by a primary key constraint.', 'SCHEMA', N'Person', 'TABLE', N'Address', 'INDEX', N'PK_Address_AddressID'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Primary key (clustered) constraint', 'SCHEMA', N'Person', 'TABLE', N'Address', 'CONSTRAINT', N'PK_Address_AddressID'
 GO
