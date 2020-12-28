6c7223a62597cd714a370ba534ca662c42e623a1
n3manj4 <n3manj4>
2020-12-28 16:04:14 +0100

    culture

---
+++ final/Tables/Production.Culture2.sql
@@ -0,0 +1,44 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [Production].[Culture2] (
+		[CultureID]        [nchar](6) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+		[Name]             [dbo].[Name] NOT NULL,
+		[ModifiedDate]     [datetime] NOT NULL,
+		[fsadf]            [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[sadfsd]           [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		CONSTRAINT [PK_Culture_CultureID]
+		PRIMARY KEY
+		CLUSTERED
+		([CultureID])
+	ON [PRIMARY]
+) ON [PRIMARY]
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Clustered index created by a primary key constraint.', 'SCHEMA', N'Production', 'TABLE', N'Culture2', 'INDEX', N'PK_Culture_CultureID'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Primary key (clustered) constraint', 'SCHEMA', N'Production', 'TABLE', N'Culture2', 'CONSTRAINT', N'PK_Culture_CultureID'
+GO
+ALTER TABLE [Production].[Culture2]
+	ADD
+	CONSTRAINT [DF_Culture_ModifiedDate]
+	DEFAULT (getdate()) FOR [ModifiedDate]
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of GETDATE()', 'SCHEMA', N'Production', 'TABLE', N'Culture2', 'CONSTRAINT', N'DF_Culture_ModifiedDate'
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [AK_Culture_Name]
+	ON [Production].[Culture2] ([Name])
+	ON [PRIMARY]
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Unique nonclustered index.', 'SCHEMA', N'Production', 'TABLE', N'Culture2', 'INDEX', N'AK_Culture_Name'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Lookup table containing the languages in which some AdventureWorks data is stored.', 'SCHEMA', N'Production', 'TABLE', N'Culture2', NULL, NULL
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Primary key for Culture records.', 'SCHEMA', N'Production', 'TABLE', N'Culture2', 'COLUMN', N'CultureID'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Culture description.', 'SCHEMA', N'Production', 'TABLE', N'Culture2', 'COLUMN', N'Name'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Date and time the record was last updated.', 'SCHEMA', N'Production', 'TABLE', N'Culture2', 'COLUMN', N'ModifiedDate'
+GO
+ALTER TABLE [Production].[Culture2] SET (LOCK_ESCALATION = TABLE)
+GO
